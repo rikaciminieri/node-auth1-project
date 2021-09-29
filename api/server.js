8fc0e2cc1ae77ff7fca1a3cb721a62d607c03c90
Rika Ciminieri <rikaciminieri@gmail.com>
2021-09-28 18:02:29 -0700

    users-model functions completed

--- api/server.js
+++ api/server.js
@@ -16,16 +16,21 @@ const cors = require("cors");
  */
 
 const server = express();
+const usersRouter = require("./users/users-router");
+const authRouter = require("./auth/auth-router");
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
+server.use("/api/users", usersRouter);
+server.use("/api/auth", authRouter);
 server.get("/", (req, res) => {
   res.json({ api: "up" });
 });
 
-server.use((err, req, res, next) => { // eslint-disable-line
+server.use((err, req, res, next) => {
+  // eslint-disable-line
   res.status(err.status || 500).json({
     message: err.message,
     stack: err.stack,
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,6 +1,22 @@
 // Require the `restricted` middleware from `auth-middleware.js`. You will need it here!
+const router = require("express").Router();
+const Users = require("./users-model");
 
+router.get("/", (req, res, next) => {
+  Users.find()
+    .then((users) => {
+      res.status(200).json(users);
+    })
+    .catch(next);
+});
 
+router.post("/", (req, res, next) => {
+  Users.add(req.body)
+    .then((newUser) => {
+      res.status(201).json(newUser);
+    })
+    .catch(next);
+});
 /**
   [GET] /api/users
 
@@ -24,5 +40,5 @@
   }
  */
 
-
 // Don't forget to add the router to the `exports` object so it can be required in other modules
+module.exports = router;
